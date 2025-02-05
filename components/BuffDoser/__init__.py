8c803bd190322f9b1a9d0db0bf2feeb0f85448ae
Richie Vos <vosrichie@gmail.com>
2025-02-04 22:58:14 -0800

    test again

--- components/BuffDoser/__init__.py
+++ components/BuffDoser/__init__.py
@@ -1,20 +1,28 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import binary_sensor, i2c, sensor
+from esphome.components import i2c
 from esphome.const import (
-    CONF_ID, 
-    CONF_TRIGGER_ID
+    CONF_ADDRESS,
+    CONF_COMMAND,
+    CONF_ID,
+    CONF_DURATION,
+    CONF_VOLUME,
 )
-# from esphome.components.MiniEncoderC.light import MiniEncoderCLightOutput
 from esphome import automation
-from esphome.components.light import LightState
+from esphome.automation import maybe_simple_id
 
+CODEOWNERS = ["@carlos-sarmiento"]
+DEPENDENCIES = ["i2c"]
 MULTI_CONF = True
 
-AUTO_LOAD = [ "binary_sensor", "sensor" ]
+CONF_VOLUME_PER_MINUTE = "volume_per_minute"
 
-# DEPENDENCIES = ['i2c']
-DEPENDENCIES = []
+buff_ns = cg.esphome_ns.namespace('buff')
+BuffDoser = buff.class_('BuffDoser', i2c.I2CDevice, cg.Component)
+
+# ezo_pmp_ns = cg.esphome_ns.namespace("ezo_pmp")
+# EzoPMP = buff_ns.class_("BuffDoser", cg.PollingComponent, i2c.I2CDevice)
+
 
 cg.add_library(
     name="StepperDriver",
@@ -68,71 +76,277 @@ cg.add_library(
     version="^1.2.3",
 )
 
-buff_ns = cg.esphome_ns.namespace('buff')
-Buff = buff.class_('Buff', i2c.I2CDevice, cg.Component)
-# CONF_ON_CLOCKWISE = "on_clockwise"
-# CONF_ON_ANTICLOCKWISE = "on_anticlockwise"
-# CONF_ENCODER_FILTER = "encoder_filter"
-# CONF_BUTTON = "button"
-# CONF_ENCODER = "encoder"
-
-# MiniEncoderCClockwiseTrigger = buff.class_(
-#     "MiniEncoderCClockwiseTrigger", automation.Trigger
-# )
-# MiniEncoderCAnticlockwiseTrigger = buff.class_(
-#     "MiniEncoderCAnticlockwiseTrigger", automation.Trigger
-# )
-
-# ENCODER_SCHEMA = cv.Schema(
-#     {
-#         cv.Optional(CONF_ON_CLOCKWISE): automation.validate_automation(
-#             {
-#                 cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-#                     MiniEncoderCClockwiseTrigger
-#                 ),
-#             }
-#         ),
-#         cv.Optional(CONF_ON_ANTICLOCKWISE): automation.validate_automation(
-#             {
-#                 cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-#                     MiniEncoderCAnticlockwiseTrigger
-#                 ),
-#             }
-#         ),
-#         cv.Optional(CONF_ENCODER_FILTER, default=1): cv.int_range(min=1, max=100),
-#     }
-# ).extend(sensor.sensor_schema())
-
-CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(Buff),
-        # cv.Optional(CONF_ENCODER): ENCODER_SCHEMA,
-        # cv.Optional(CONF_BUTTON): binary_sensor.binary_sensor_schema(),
-    }
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(BuffDoser),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(cv.polling_component_schema("60s"))
+    .extend(i2c.i2c_device_schema(103))
 )
-# ).extend(i2c.i2c_device_schema(0x42))
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    # await i2c.register_i2c_device(var, config)
+    await i2c.register_i2c_device(var, config)
 
-    # if CONF_ENCODER in config:
-    #     sens = await sensor.new_sensor(config[CONF_ENCODER])
-    #     cg.add(var.set_encoder(sens))
 
-    #     encoderConfig = config[CONF_ENCODER]
+BUFF_DOSER_NO_ARGS_ACTION_SCHEMA = maybe_simple_id(
+    {
+        cv.Required(CONF_ID): cv.use_id(BuffDoser),
+    }
+)
 
-    #     if CONF_ENCODER_FILTER in encoderConfig:
-    #         cg.add(var.set_encoder_filter(encoderConfig[CONF_ENCODER_FILTER]))
+# Actions that do not require more arguments
 
-    #     for conf in encoderConfig.get(CONF_ON_CLOCKWISE, []):
-    #         trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
-    #         await automation.build_automation(trigger, [], conf)
-    #     for conf in encoderConfig.get(CONF_ON_ANTICLOCKWISE, []):
-    #         trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
-    #         await automation.build_automation(trigger, [], conf)
+BuffDoserFindAction = buff_ns.class_("BuffDoserFindAction", automation.Action)
+BuffDoserClearTotalVolumeDispensedAction = buff_ns.class_(
+    "BuffDoserClearTotalVolumeDispensedAction", automation.Action
+)
+BuffDoserClearCalibrationAction = buff_ns.class_(
+    "BuffDoserClearCalibrationAction", automation.Action
+)
+BuffDoserPauseDosingAction = buff_ns.class_(
+    "BuffDoserPauseDosingAction", automation.Action
+)
+BuffDoserStopDosingAction = buff_ns.class_("BuffDoserStopDosingAction", automation.Action)
+BuffDoserDoseContinuouslyAction = buff_ns.class_(
+    "BuffDoserDoseContinuouslyAction", automation.Action
+)
 
-    # if CONF_BUTTON in config:
-    #     button = await binary_sensor.new_binary_sensor(config[CONF_BUTTON])
-    #     cg.add(var.set_button(button))
+# Actions that require more arguments
+BuffDoserDoseVolumeAction = buff_ns.class_("BuffDoserDoseVolumeAction", automation.Action)
+BuffDoserDoseVolumeOverTimeAction = buff_ns.class_(
+    "BuffDoserDoseVolumeOverTimeAction", automation.Action
+)
+BuffDoserDoseWithConstantFlowRateAction = buff_ns.class_(
+    "BuffDoserDoseWithConstantFlowRateAction", automation.Action
+)
+BuffDoserSetCalibrationVolumeAction = buff_ns.class_(
+    "BuffDoserSetCalibrationVolumeAction", automation.Action
+)
+BuffDoserChangeI2CAddressAction = buff_ns.class_(
+    "BuffDoserChangeI2CAddressAction", automation.Action
+)
+BuffDoserArbitraryCommandAction = buff_ns.class_(
+    "BuffDoserArbitraryCommandAction", automation.Action
+)
+
+
+@automation.register_action(
+    "buff_doser.find", BuffDoserFindAction, BUFF_DOSER_NO_ARGS_ACTION_SCHEMA
+)
+async def buff_doser_find_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    return cg.new_Pvariable(action_id, template_arg, paren)
+
+
+@automation.register_action(
+    "buff_doser.dose_continuously",
+    BuffDoserDoseContinuouslyAction,
+    BUFF_DOSER_NO_ARGS_ACTION_SCHEMA,
+)
+async def buff_doser_dose_continuously_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    return cg.new_Pvariable(action_id, template_arg, paren)
+
+
+@automation.register_action(
+    "buff_doser.clear_total_volume_dosed",
+    BuffDoserClearTotalVolumeDispensedAction,
+    buff_doser_NO_ARGS_ACTION_SCHEMA,
+)
+async def buff_doser_clear_total_volume_dosed_to_code(
+    config, action_id, template_arg, args
+):
+    paren = await cg.get_variable(config[CONF_ID])
+    return cg.new_Pvariable(action_id, template_arg, paren)
+
+
+@automation.register_action(
+    "buff_doser.clear_calibration",
+    BuffDoserClearCalibrationAction,
+    BUFF_DOSER_NO_ARGS_ACTION_SCHEMA,
+)
+async def buff_doser_clear_calibration_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    return cg.new_Pvariable(action_id, template_arg, paren)
+
+
+@automation.register_action(
+    "buff_doser.pause_dosing", BuffDoserPauseDosingAction, BUFF_DOSER_NO_ARGS_ACTION_SCHEMA
+)
+async def buff_doser_pause_dosing_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    return cg.new_Pvariable(action_id, template_arg, paren)
+
+
+@automation.register_action(
+    "buff_doser.stop_dosing", BuffDoserStopDosingAction, BUFF_DOSER_NO_ARGS_ACTION_SCHEMA
+)
+async def buff_doser_stop_dosing_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    return cg.new_Pvariable(action_id, template_arg, paren)
+
+
+# Actions that require Multiple Args
+
+BUFF_DOSER_DOSE_VOLUME_ACTION_SCHEMA = cv.All(
+    {
+        cv.Required(CONF_ID): cv.use_id(BuffDoser),
+        cv.Required(CONF_VOLUME): cv.templatable(
+            cv.float_range()
+        ),  # Any way to represent as proper volume (vs. raw int)
+    }
+)
+
+
+@automation.register_action(
+    "buff_doser.dose_volume", BuffDoserDoseVolumeAction, BUFF_DOSER_DOSE_VOLUME_ACTION_SCHEMA
+)
+async def buff_doser_dose_volume_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+
+    template_ = await cg.templatable(config[CONF_VOLUME], args, cg.double)
+    cg.add(var.set_volume(template_))
+
+    return var
+
+
+BUFF_DOSER_DOSE_VOLUME_OVER_TIME_ACTION_SCHEMA = cv.All(
+    {
+        cv.Required(CONF_ID): cv.use_id(BuffDoser),
+        cv.Required(CONF_VOLUME): cv.templatable(
+            cv.float_range()
+        ),  # Any way to represent as proper volume (vs. raw int)
+        cv.Required(CONF_DURATION): cv.templatable(
+            cv.int_range(1)
+        ),  # Any way to represent it as minutes (vs. raw int)
+    }
+)
+
+
+@automation.register_action(
+    "buff_doser.dose_volume_over_time",
+    BuffDoserDoseVolumeOverTimeAction,
+    BUFF_DOSER_DOSE_VOLUME_OVER_TIME_ACTION_SCHEMA,
+)
+async def buff_doser_dose_volume_over_time_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+
+    template_ = await cg.templatable(config[CONF_VOLUME], args, cg.double)
+    cg.add(var.set_volume(template_))
+
+    template_ = await cg.templatable(config[CONF_DURATION], args, int)
+    cg.add(var.set_duration(template_))
+
+    return var
+
+
+BUFF_DOSER_DOSE_WITH_CONSTANT_FLOW_RATE_ACTION_SCHEMA = cv.All(
+    {
+        cv.Required(CONF_ID): cv.use_id(BuffDoser),
+        cv.Required(CONF_VOLUME_PER_MINUTE): cv.templatable(
+            cv.float_range()
+        ),  # Any way to represent as proper volume (vs. raw int)
+        cv.Required(CONF_DURATION): cv.templatable(
+            cv.int_range(1)
+        ),  # Any way to represent it as minutes (vs. raw int)
+    }
+)
+
+
+@automation.register_action(
+    "buff_doser.dose_with_constant_flow_rate",
+    BuffDoserDoseWithConstantFlowRateAction,
+    BUFF_DOSER_DOSE_WITH_CONSTANT_FLOW_RATE_ACTION_SCHEMA,
+)
+async def buff_doser_dose_with_constant_flow_rate_to_code(
+    config, action_id, template_arg, args
+):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+
+    template_ = await cg.templatable(config[CONF_VOLUME_PER_MINUTE], args, cg.double)
+    cg.add(var.set_volume(template_))
+
+    template_ = await cg.templatable(config[CONF_DURATION], args, int)
+    cg.add(var.set_duration(template_))
+
+    return var
+
+
+BUFF_DOSER_SET_CALIBRATION_VOLUME_ACTION_SCHEMA = cv.All(
+    {
+        cv.Required(CONF_ID): cv.use_id(BuffDoser),
+        cv.Required(CONF_VOLUME): cv.templatable(
+            cv.float_range()
+        ),  # Any way to represent as proper volume (vs. raw int)
+    }
+)
+
+
+@automation.register_action(
+    "buff_doser.set_calibration_volume",
+    BuffDoserSetCalibrationVolumeAction,
+    BUFF_DOSER_SET_CALIBRATION_VOLUME_ACTION_SCHEMA,
+)
+async def buff_doser_set_calibration_volume_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+
+    template_ = await cg.templatable(config[CONF_VOLUME], args, cg.double)
+    cg.add(var.set_volume(template_))
+
+    return var
+
+
+BUFF_DOSER_CHANGE_I2C_ADDRESS_ACTION_SCHEMA = cv.All(
+    {
+        cv.Required(CONF_ID): cv.use_id(BuffDoser),
+        cv.Required(CONF_ADDRESS): cv.templatable(cv.int_range(min=1, max=127)),
+    }
+)
+
+
+@automation.register_action(
+    "buff_doser.change_i2c_address",
+    BuffDoserChangeI2CAddressAction,
+    BUFF_DOSER_CHANGE_I2C_ADDRESS_ACTION_SCHEMA,
+)
+async def buff_doser_change_i2c_address_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+
+    template_ = await cg.templatable(config[CONF_ADDRESS], args, cg.double)
+    cg.add(var.set_address(template_))
+
+    return var
+
+
+BUFF_DOSER_ARBITRARY_COMMAND_ACTION_SCHEMA = cv.All(
+    {
+        cv.Required(CONF_ID): cv.use_id(BuffDoser),
+        cv.Required(CONF_COMMAND): cv.templatable(cv.string_strict),
+    }
+)
+
+
+@automation.register_action(
+    "buff_doser.arbitrary_command",
+    BuffDoserArbitraryCommandAction,
+    BUFF_DOSER_ARBITRARY_COMMAND_ACTION_SCHEMA,
+)
+async def buff_doser_arbitrary_command_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+
+    template_ = await cg.templatable(config[CONF_COMMAND], args, cg.std_string)
+    cg.add(var.set_command(template_))
+
+    return var
---
+++ components/BuffDoser/buff_doser.h
@@ -0,0 +1,252 @@
+#pragma once
+
+#include "esphome/core/defines.h"
+#include "esphome/core/component.h"
+#include "esphome/core/automation.h"
+#include "esphome/components/i2c/i2c.h"
+
+#ifdef USE_BINARY_SENSOR
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#endif
+
+#ifdef USE_SENSOR
+#include "esphome/components/sensor/sensor.h"
+#endif
+
+#ifdef USE_TEXT_SENSOR
+#include "esphome/components/text_sensor/text_sensor.h"
+#endif
+
+namespace esphome {
+namespace buff {
+
+class BuffDoser : public PollingComponent, public i2c::I2CDevice {
+ public:
+  void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::DATA; };
+
+  void loop() override;
+  void update() override;
+
+#ifdef USE_SENSOR
+  void set_current_volume_dosed(sensor::Sensor *current_volume_dosed) { current_volume_dosed_ = current_volume_dosed; }
+  void set_total_volume_dosed(sensor::Sensor *total_volume_dosed) { total_volume_dosed_ = total_volume_dosed; }
+  void set_absolute_total_volume_dosed(sensor::Sensor *absolute_total_volume_dosed) {
+    absolute_total_volume_dosed_ = absolute_total_volume_dosed;
+  }
+  void set_pump_voltage(sensor::Sensor *pump_voltage) { pump_voltage_ = pump_voltage; }
+  void set_last_volume_requested(sensor::Sensor *last_volume_requested) {
+    last_volume_requested_ = last_volume_requested;
+  }
+  void set_max_flow_rate(sensor::Sensor *max_flow_rate) { max_flow_rate_ = max_flow_rate; }
+#endif
+
+#ifdef USE_BINARY_SENSOR
+  void set_is_dosing(binary_sensor::BinarySensor *is_dosing) { is_dosing_ = is_dosing; }
+  void set_is_paused(binary_sensor::BinarySensor *is_paused) { is_paused_ = is_paused; }
+#endif
+
+#ifdef USE_TEXT_SENSOR
+  void set_dosing_mode(text_sensor::TextSensor *dosing_mode) { dosing_mode_ = dosing_mode; }
+  void set_calibration_status(text_sensor::TextSensor *calibration_status) { calibration_status_ = calibration_status; }
+#endif
+
+  // Actions for BuffDoser
+  void find();
+  void dose_continuously();
+  void dose_volume(double volume);
+  void dose_volume_over_time(double volume, int duration);
+  void dose_with_constant_flow_rate(double volume, int duration);
+  void set_calibration_volume(double volume);
+  void clear_total_volume_dosed();
+  void clear_calibration();
+  void pause_dosing();
+  void stop_dosing();
+  void change_i2c_address(int address);
+  void exec_arbitrary_command(const std::basic_string<char> &command);
+
+ protected:
+  uint32_t start_time_ = 0;
+  uint32_t wait_time_ = 0;
+  bool is_waiting_ = false;
+  bool is_first_read_ = true;
+
+  uint16_t next_command_ = 0;
+  double next_command_volume_ = 0;  // might be negative
+  int next_command_duration_ = 0;
+
+  uint16_t next_command_queue_[10];
+  double next_command_volume_queue_[10];
+  int next_command_duration_queue_[10];
+  int next_command_queue_head_ = 0;
+  int next_command_queue_last_ = 0;
+  int next_command_queue_length_ = 0;
+
+  uint16_t current_command_ = 0;
+  bool is_paused_flag_ = false;
+  bool is_dosing_flag_ = false;
+
+  const char *arbitrary_command_{nullptr};
+
+  void send_next_command_();
+  void read_command_result_();
+  void clear_current_command_();
+  void queue_command_(uint16_t command, double volume, int duration, bool should_schedule);
+  void pop_next_command_();
+  uint16_t peek_next_command_();
+
+#ifdef USE_SENSOR
+  sensor::Sensor *current_volume_dosed_{nullptr};
+  sensor::Sensor *total_volume_dosed_{nullptr};
+  sensor::Sensor *absolute_total_volume_dosed_{nullptr};
+  sensor::Sensor *pump_voltage_{nullptr};
+  sensor::Sensor *max_flow_rate_{nullptr};
+  sensor::Sensor *last_volume_requested_{nullptr};
+#endif
+
+#ifdef USE_BINARY_SENSOR
+  binary_sensor::BinarySensor *is_dosing_{nullptr};
+  binary_sensor::BinarySensor *is_paused_{nullptr};
+#endif
+
+#ifdef USE_TEXT_SENSOR
+  text_sensor::TextSensor *dosing_mode_{nullptr};
+  text_sensor::TextSensor *calibration_status_{nullptr};
+#endif
+};
+
+// Action Templates
+template<typename... Ts> class BuffDoserFindAction : public Action<Ts...> {
+ public:
+  BuffDoserFindAction(BuffDoser *buff_doser) : BuffDoser_(buff_doser) {}
+
+  void play(Ts... x) override { this->buff_doser_->find(); }
+
+ protected:
+  BuffDoser *buff_doser_;
+};
+
+template<typename... Ts> class BuffDoserDoseContinuouslyAction : public Action<Ts...> {
+ public:
+  BuffDoserDoseContinuouslyAction(BuffDoser *buff_doser) : buff_doser_(buff_doser) {}
+
+  void play(Ts... x) override { this->buff_doser_->dose_continuously(); }
+
+ protected:
+  BuffDoser *buff_doser_;
+};
+
+template<typename... Ts> class BuffDoserDoseVolumeAction : public Action<Ts...> {
+ public:
+  BuffDoserDoseVolumeAction(BuffDoser *buff_doser) : buff_doser_(buff_doser) {}
+
+  void play(Ts... x) override { this->buff_doser_->dose_volume(this->volume_.value(x...)); }
+  TEMPLATABLE_VALUE(double, volume)
+
+ protected:
+  BuffDoser *buff_doser_;
+};
+
+template<typename... Ts> class BuffDoserDoseVolumeOverTimeAction : public Action<Ts...> {
+ public:
+  BuffDoserDoseVolumeOverTimeAction(BuffDoser *buff_doser) : buff_doser_(buff_doser) {}
+
+  void play(Ts... x) override {
+    this->buff_doser_->dose_volume_over_time(this->volume_.value(x...), this->duration_.value(x...));
+  }
+  TEMPLATABLE_VALUE(double, volume)
+  TEMPLATABLE_VALUE(int, duration)
+
+ protected:
+  BuffDoser *buff_doser_;
+};
+
+template<typename... Ts> class BuffDoserDoseWithConstantFlowRateAction : public Action<Ts...> {
+ public:
+  BuffDoserDoseWithConstantFlowRateAction(BuffDoser *buff_doser) : buff_doser_(buff_doser) {}
+
+  void play(Ts... x) override {
+    this->buff_doser_->dose_with_constant_flow_rate(this->volume_.value(x...), this->duration_.value(x...));
+  }
+  TEMPLATABLE_VALUE(double, volume)
+  TEMPLATABLE_VALUE(int, duration)
+
+ protected:
+  BuffDoser *buff_doser_;
+};
+
+template<typename... Ts> class BuffDoserSetCalibrationVolumeAction : public Action<Ts...> {
+ public:
+  BuffDoserSetCalibrationVolumeAction(BuffDoser *buff_doser) : buff_doser_(buff_doser) {}
+
+  void play(Ts... x) override { this->buff_doser_->set_calibration_volume(this->volume_.value(x...)); }
+  TEMPLATABLE_VALUE(double, volume)
+
+ protected:
+  BuffDoser *buff_doser_;
+};
+
+template<typename... Ts> class BuffDoserClearTotalVolumeDispensedAction : public Action<Ts...> {
+ public:
+  BuffDoserClearTotalVolumeDispensedAction(BuffDoser *buff_doser) : buff_doser_(buff_doser) {}
+
+  void play(Ts... x) override { this->buff_doser_->clear_total_volume_dosed(); }
+
+ protected:
+  BuffDoser *buff_doser_;
+};
+
+template<typename... Ts> class BuffDoserClearCalibrationAction : public Action<Ts...> {
+ public:
+  BuffDoserClearCalibrationAction(BuffDoser *buff_doser) : buff_doser_(buff_doser) {}
+
+  void play(Ts... x) override { this->buff_doser_->clear_calibration(); }
+
+ protected:
+  BuffDoser *buff_doser_;
+};
+
+template<typename... Ts> class BuffDoserPauseDosingAction : public Action<Ts...> {
+ public:
+  BuffDoserPauseDosingAction(BuffDoser *buff_doser) : buff_doser_(buff_doser) {}
+
+  void play(Ts... x) override { this->buff_doser_->pause_dosing(); }
+
+ protected:
+  BuffDoser *buff_doser_;
+};
+
+template<typename... Ts> class BuffDoserStopDosingAction : public Action<Ts...> {
+ public:
+  BuffDoserStopDosingAction(BuffDoser *buff_doser) : buff_doser_(buff_doser) {}
+
+  void play(Ts... x) override { this->buff_doser_->stop_dosing(); }
+
+ protected:
+  BuffDoser *buff_doser_;
+};
+
+template<typename... Ts> class BuffDoserChangeI2CAddressAction : public Action<Ts...> {
+ public:
+  BuffDoserChangeI2CAddressAction(BuffDoser *buff_doser) : buff_doser_(buff_doser) {}
+
+  void play(Ts... x) override { this->buff_doser_->change_i2c_address(this->address_.value(x...)); }
+  TEMPLATABLE_VALUE(int, address)
+
+ protected:
+  BuffDoser *buff_doser_;
+};
+
+template<typename... Ts> class BuffDoserArbitraryCommandAction : public Action<Ts...> {
+ public:
+  BuffDoserArbitraryCommandAction(BuffDoser *buff_doser) : buff_doser_(buff_doser) {}
+
+  void play(Ts... x) override { this->buff_doser_->exec_arbitrary_command(this->command_.value(x...)); }
+  TEMPLATABLE_VALUE(std::string, command)
+
+ protected:
+  BuffDoser *buff_doser_;
+};
+
+}  // namespace buff
+}  // namespace esphome
