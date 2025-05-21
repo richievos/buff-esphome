0adf9acd01c65daead3aac2ba018aaa2dd1973c6
Richie Vos <vosrichie@gmail.com>
2025-05-20 23:09:10 -0700

    test again

--- src/main.cpp
+++ src/main.cpp
@@ -1,93 +1,93 @@
 #ifdef STANDALONE_REEFBUFF
-// #include "std-backport.h"
+// // #include "std-backport.h"
 
-// #include <cmath>
-// #include <cstring>
-#include <functional>
-#include <memory>
+// // #include <cmath>
+// // #include <cstring>
+// #include <functional>
+// #include <memory>
 
-#include <Arduino.h>
-#include <WiFi.h>
+// #include <Arduino.h>
+// #include <WiFi.h>
 
-#include "mks-skinny/I2SOut.h"
+// #include "mks-skinny/I2SOut.h"
 
-// Buff Libraries
-#include "readings/alk-measure.h"
-#include "controller.h"
-#include "doser/doser.h"
-#include "inputs.h"
-#include "mqtt-publish.h"
-#include "mqtt.h"
-#include "mywifi.h"
-#include "ntp.h"
-#include "ota.h"
-#include "readings/ph-controller.h"
+// // Buff Libraries
+// #include "readings/alk-measure.h"
+// #include "controller.h"
+// #include "doser/doser.h"
+// #include "inputs.h"
+// #include "mqtt-publish.h"
+// #include "mqtt.h"
+// #include "mywifi.h"
+// #include "ntp.h"
+// #include "ota.h"
+// #include "readings/ph-controller.h"
 
-namespace buff {
-/*******************************
- * Shared vars
- *******************************/
-const size_t STANDARD_PH_MAVG_LENGTH = 30;
-auto phReader = std::make_shared<ph::controller::PHReader>(inputs::phReadConfig, inputs::phCalibrator);
-ph::controller::PHReadingStats<STANDARD_PH_MAVG_LENGTH> phReadingStats;
+// namespace buff {
+// /*******************************
+//  * Shared vars
+//  *******************************/
+// const size_t STANDARD_PH_MAVG_LENGTH = 30;
+// auto phReader = std::make_shared<ph::controller::PHReader>(inputs::phReadConfig, inputs::phCalibrator);
+// ph::controller::PHReadingStats<STANDARD_PH_MAVG_LENGTH> phReadingStats;
 
-auto mqttBroker = std::make_shared<MqttBroker>(inputs::MQTT_BROKER_PORT);
-auto mqttClient = std::make_shared<MqttClient>(mqttBroker.get());
+// auto mqttBroker = std::make_shared<MqttBroker>(inputs::MQTT_BROKER_PORT);
+// auto mqttClient = std::make_shared<MqttClient>(mqttBroker.get());
 
-auto publisher = std::make_shared<mqtt::MQTTPublisher>(mqttClient);
+// auto publisher = std::make_shared<mqtt::MQTTPublisher>(mqttClient);
 
-std::shared_ptr<NTPClient> ntpClient;
-std::shared_ptr<buff_time::TimeWrapper> timeClient;
+// std::shared_ptr<NTPClient> ntpClient;
+// std::shared_ptr<buff_time::TimeWrapper> timeClient;
 
-std::shared_ptr<doser::BuffDosers> buffDosers;
+// std::shared_ptr<doser::BuffDosers> buffDosers;
 
-/**************************
- * Setup & Loop
- **************************/
-void setup() {
-    Serial.begin(115200);
-    Wire.begin(inputs::PIN_CONFIG.I2C_SDA, inputs::PIN_CONFIG.I2C_SCL);
+// /**************************
+//  * Setup & Loop
+//  **************************/
+// void setup() {
+//     Serial.begin(115200);
+//     Wire.begin(inputs::PIN_CONFIG.I2C_SDA, inputs::PIN_CONFIG.I2C_SCL);
 
-    richiev::connectWifi(inputs::hostname, inputs::wifiSSID, inputs::wifiPassword);
-    richiev::ota::setupOTA(inputs::hostname);
+//     richiev::connectWifi(inputs::hostname, inputs::wifiSSID, inputs::wifiPassword);
+//     richiev::ota::setupOTA(inputs::hostname);
 
-    buffDosers = std::move(doser::setupDosers(inputs::PIN_CONFIG.STEPPER_DISABLE_PIN, inputs::doserInstances, inputs::doserSteppers));
-    // TODO: make this configurable
-    setupPH_RoboTankPHBoard();
+//     buffDosers = std::move(doser::setupDosers(inputs::PIN_CONFIG.STEPPER_DISABLE_PIN, inputs::doserInstances, inputs::doserSteppers));
+//     // TODO: make this configurable
+//     setupPH_RoboTankPHBoard();
 
-    // trigger a NTP refresh
-    ntpClient = std::move(ntp::setupNTP());
-    timeClient = std::make_shared<ntp::NTPTimeWrapper>(ntpClient);
+//     // trigger a NTP refresh
+//     ntpClient = std::move(ntp::setupNTP());
+//     timeClient = std::make_shared<ntp::NTPTimeWrapper>(ntpClient);
 
-    controller::setupController(mqttBroker, mqttClient, buffDosers, phReader, inputs::alkMeasureConf, publisher, timeClient);
-}
+//     controller::setupController(mqttBroker, mqttClient, buffDosers, phReader, inputs::alkMeasureConf, publisher, timeClient);
+// }
 
-void loop() {
-    auto phReadingPtr = phReader->readNewPHSignalIfTimeAndUpdate<STANDARD_PH_MAVG_LENGTH>(phReadingStats);
-    if (phReadingPtr != nullptr) {
-        phReadingPtr->asOfAdjustedSec = timeClient->getAdjustedTimeSeconds();
-        publisher->publishPH(*phReadingPtr);
-    }
+// void loop() {
+//     auto phReadingPtr = phReader->readNewPHSignalIfTimeAndUpdate<STANDARD_PH_MAVG_LENGTH>(phReadingStats);
+//     if (phReadingPtr != nullptr) {
+//         phReadingPtr->asOfAdjustedSec = timeClient->getAdjustedTimeSeconds();
+//         publisher->publishPH(*phReadingPtr);
+//     }
 
-    ntp::loopNTP(ntpClient);
+//     ntp::loopNTP(ntpClient);
 
-    controller::loopController();
+//     controller::loopController();
 
-    richiev::mqtt::loopMQTT(mqttBroker, mqttClient);
-    richiev::ota::loopOTA();
-}
+//     richiev::mqtt::loopMQTT(mqttBroker, mqttClient);
+//     richiev::ota::loopOTA();
+// }
 
-}  // namespace buff
+// }  // namespace buff
 
-/**************************
- * Setup & Loop
- **************************/
-void setup() {
-    buff::setup();
-}
+// /**************************
+//  * Setup & Loop
+//  **************************/
+// void setup() {
+//     buff::setup();
+// }
 
-void loop() {
-    buff::loop();
-}
+// void loop() {
+//     buff::loop();
+// }
 
 #endif
