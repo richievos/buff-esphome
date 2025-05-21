ad5a22b0a2d56121d35e775e17be03d028a90cb1
Richie Vos <vosrichie@gmail.com>
2025-05-21 01:40:24 -0700

    test again

--- esphome/components/buff_doser/buff_doser.cpp
+++ esphome/components/buff_doser/buff_doser.cpp
@@ -5,6 +5,57 @@
 
 #include "inputs.h"
 
+//////////////////////////////////////////////////////////////
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include "time-common.h"
+
+#include <memory>
+
+namespace ntp {
+
+WiFiUDP ntpUDP;
+
+std::unique_ptr<NTPClient> setupNTP() {
+    auto timeClient = std::make_unique<NTPClient>(ntpUDP);
+
+    Serial.println("Setting up ntp client");
+    timeClient->setUpdateInterval(1000 * 60 * 30);
+    timeClient->begin();
+    timeClient->setTimeOffset(0);
+
+    const int num_tries = 5;
+    for (int i = 0; i < num_tries && !timeClient->update(); i++) {
+        timeClient->forceUpdate();
+        delay(1000);
+    }
+
+    Serial.println("Current time: ");
+    Serial.println(timeClient->getFormattedTime());
+
+    return std::move(timeClient);
+}
+
+void loopNTP(std::shared_ptr<NTPClient> timeClient) {
+    // this will trigger a time refresh if needed
+    // WARNING: this is sync and blocking
+    timeClient->update();
+}
+
+class NTPTimeWrapper : public buff::buff_time::TimeWrapper {
+   private:
+    const std::shared_ptr<NTPClient> _timeClient;
+
+   public:
+    NTPTimeWrapper(std::shared_ptr<NTPClient> timeClient): _timeClient(timeClient) {}
+    virtual unsigned long getAdjustedTimeSeconds() {
+        return _timeClient->getEpochTime();
+    }
+};
+
+}  // namespace ntp
+//////////////////////////////////////////////////////////////
+
 namespace esphome {
 namespace buff {
 
--- esphome/components/buff_doser/ntp.h
+++ esphome/components/buff_doser/ntp.h
@@ -1,51 +1,51 @@
 // based on https://github.com/PaulStoffregen/Time/blob/master/examples/TimeNTP_ESP8266WiFi/TimeNTP_ESP8266WiFi.ino
 #pragma once
 
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-#include "time-common.h"
+// #include <NTPClient.h>
+// #include <WiFiUdp.h>
+// #include "time-common.h"
 
-#include <memory>
+// #include <memory>
 
-namespace ntp {
+// namespace ntp {
 
-WiFiUDP ntpUDP;
+// WiFiUDP ntpUDP;
 
-std::unique_ptr<NTPClient> setupNTP() {
-    auto timeClient = std::make_unique<NTPClient>(ntpUDP);
+// std::unique_ptr<NTPClient> setupNTP() {
+//     auto timeClient = std::make_unique<NTPClient>(ntpUDP);
 
-    Serial.println("Setting up ntp client");
-    timeClient->setUpdateInterval(1000 * 60 * 30);
-    timeClient->begin();
-    timeClient->setTimeOffset(0);
+//     Serial.println("Setting up ntp client");
+//     timeClient->setUpdateInterval(1000 * 60 * 30);
+//     timeClient->begin();
+//     timeClient->setTimeOffset(0);
 
-    const int num_tries = 5;
-    for (int i = 0; i < num_tries && !timeClient->update(); i++) {
-        timeClient->forceUpdate();
-        delay(1000);
-    }
+//     const int num_tries = 5;
+//     for (int i = 0; i < num_tries && !timeClient->update(); i++) {
+//         timeClient->forceUpdate();
+//         delay(1000);
+//     }
 
-    Serial.println("Current time: ");
-    Serial.println(timeClient->getFormattedTime());
+//     Serial.println("Current time: ");
+//     Serial.println(timeClient->getFormattedTime());
 
-    return std::move(timeClient);
-}
+//     return std::move(timeClient);
+// }
 
-void loopNTP(std::shared_ptr<NTPClient> timeClient) {
-    // this will trigger a time refresh if needed
-    // WARNING: this is sync and blocking
-    timeClient->update();
-}
+// void loopNTP(std::shared_ptr<NTPClient> timeClient) {
+//     // this will trigger a time refresh if needed
+//     // WARNING: this is sync and blocking
+//     timeClient->update();
+// }
 
-class NTPTimeWrapper : public buff::buff_time::TimeWrapper {
-   private:
-    const std::shared_ptr<NTPClient> _timeClient;
+// class NTPTimeWrapper : public buff::buff_time::TimeWrapper {
+//    private:
+//     const std::shared_ptr<NTPClient> _timeClient;
 
-   public:
-    NTPTimeWrapper(std::shared_ptr<NTPClient> timeClient): _timeClient(timeClient) {}
-    virtual unsigned long getAdjustedTimeSeconds() {
-        return _timeClient->getEpochTime();
-    }
-};
+//    public:
+//     NTPTimeWrapper(std::shared_ptr<NTPClient> timeClient): _timeClient(timeClient) {}
+//     virtual unsigned long getAdjustedTimeSeconds() {
+//         return _timeClient->getEpochTime();
+//     }
+// };
 
-}  // namespace ntp
+// }  // namespace ntp
